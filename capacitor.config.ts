74669fe673ad610923116bde4dfa0bfe2674b418
AurecchiaP <paolo@aurecchia.ch>
2022-08-12 22:53:32 +0200

    update ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.card.learner.app',
   appName: 'Card Learner',
   webDir: 'dist/card-learner',
   bundledWebRuntime: false
