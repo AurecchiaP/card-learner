540eca2803bf39c038ceda8086cfcc1b3fce7d7d
Paolo Aurecchia <paolo@aurecchia.ch>
2022-08-12 18:06:59 +0200

    added icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'card-learner',
+  appName: 'Card Learner',
   webDir: 'dist/card-learner',
   bundledWebRuntime: false
 };
